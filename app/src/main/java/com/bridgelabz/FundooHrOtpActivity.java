105da677b8b142693e9e8861d952e36c41f6d7fe
ESHVAR289 <eshvar89@hotmail.com>
2016-07-03 18:09:03 +0530

    Created the project and added the login and otp activity also created the respective packages.

---
+++ app/src/main/java/com/bridgelabz/FundooHrOtpActivity.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FundooHrOtpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fundoo_hr_otp);
+    }
+}
---
+++ app/src/main/java/com/bridgelabz/restservice/ApiClient.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.restservice;
+
+/**
+ * Created by eshvar289 on 3/7/16.
+ */
+
+public class ApiClient {
+}
