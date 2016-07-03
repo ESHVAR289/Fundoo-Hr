92f624e6b6feef20b23eff656b1a7f6a0d1a2aac
ESHVAR289 <eshvar89@hotmail.com>
2016-07-03 18:45:53 +0530

    Created an App.java class which extends the Application class and created an instance of the interface of the NetComponent and Create a public method called getNetComponent() that will return the netcomponent variable whenever we require this in our App.

---
+++ app/src/main/java/com/bridgelabz/App.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+import android.app.Application;
+
+import com.bridgelabz.dagger.component.NetComponent;
+import com.bridgelabz.dagger.module.AppModule;
+import com.bridgelabz.dagger.module.NetModule;
+
+/**
+ * Created by eshvar289 on 3/7/16.
+ */
+
+public class App extends Application {
+
+    private NetComponent mNetComponent;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mNetComponent = DaggerNetComponent.builder()
+                        .appModule(new AppModule(this))
+                        .netModule(new NetModule("http://api.github.com"))
+                        .build;
+    }
+
+    public NetComponent getmNetComponent() {
+        return mNetComponent;
+    }
+}
