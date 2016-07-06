d79b4c9fbb94ad6f3f1ea45e1ddfb3b187f52173
ESHVAR MALI <eshvar89@hotmail.com>
2016-07-06 12:45:47 +0530

    Added methods  which are useful for the post call . Which are passing the information along with the url.

--- app/src/main/java/com/bridgelabz/restservice/RestApi.java
+++ app/src/main/java/com/bridgelabz/restservice/RestApi.java
@@ -1,11 +1,16 @@
 package com.bridgelabz.restservice;
 
 import com.bridgelabz.Post;
+import com.bridgelabz.model.MessageGson;
+import com.bridgelabz.model.MobileAndOtpModel;
+import com.bridgelabz.model.MobileNoOtpGson;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 /**
@@ -15,4 +20,16 @@ import retrofit2.http.POST;
 public interface RestApi {
     @GET("/face_rest.json")
     Call<List<Post>> getPosts();
+
+    @Headers("Content-Type: application/json")
+    @POST("sendmsg/")
+    Call<MessageGson> getMessage(@Body MessageGson messageGson);
+
+    @Headers("Content-Type: application/json")
+    @POST("otp/")
+    Call<MobileNoOtpGson> getMobileNoStatus(@Body MobileAndOtpModel mobileAndOtpModel);
+
+    @Headers("Content-Type: application/json")
+    @POST("verify/")
+    Call<MobileNoOtpGson> getOtpStatus(@Body MobileAndOtpModel mobileAndOtpModel);
 }
