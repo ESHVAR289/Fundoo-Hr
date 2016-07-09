16b30e998ea63b711ff092bf757b5af16ac68521
ESHVAR MALI <eshvar89@hotmail.com>
2016-07-09 13:48:10 +0530

    implemented the onclickListener on the imgSearch,imgBtnEditTime,imgBtnEditDate.

--- app/src/main/java/com/bridgelabz/FundooHrSearchActivity.java
+++ app/src/main/java/com/bridgelabz/FundooHrSearchActivity.java
@@ -1,12 +1,16 @@
 package com.bridgelabz;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.bridgelabz.model.TimeEntryGson;
@@ -14,6 +18,7 @@ import com.bridgelabz.model.TimeEntryMessage;
 import com.bridgelabz.restservice.RestApi;
 import com.bridgelabz.util.DateFormater;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.inject.Inject;
@@ -23,13 +28,15 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class FundooHrSearchActivity extends AppCompatActivity {
+public class FundooHrSearchActivity extends AppCompatActivity implements View.OnClickListener {
     @Inject
     Retrofit retrofit;
 
-    EditText etSearch, etDate, etTime;
-    ImageButton imgBtnSearch;
+    EditText etSearch, etDate, etTime, etEditDate, etEditTime;
+    ImageButton imgBtnSearch, imgBtnTxtEditDate, imgBtnTxtEditTime, imgBtnEditDate, imgBtnEditTime;
     TextView txtJson, txtTime;
+    Calendar calendar;
+    private int mYear, mMonth, mDay, mHour, mMinute,mSeconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,38 +49,106 @@ public class FundooHrSearchActivity extends AppCompatActivity {
 
         etSearch = (EditText) findViewById(R.id.etSearch);
         imgBtnSearch = (ImageButton) findViewById(R.id.imgBtnSearch);
+        imgBtnEditTime = (ImageButton) findViewById(R.id.btnEditTime);
+        imgBtnEditDate = (ImageButton) findViewById(R.id.btnEditDate);
         txtJson = (TextView) findViewById(R.id.txtJson);
         txtTime = (TextView) findViewById(R.id.txtTime);
         etDate = (EditText) findViewById(R.id.etDate);
         etTime = (EditText) findViewById(R.id.etTime);
-
-        imgBtnSearch.setOnClickListener(new View.OnClickListener() {
+        /*imgBtnEditTime.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String mobile_no = "+919923911289"/*getIntent().getStringExtra("mobile")*/;
-                String search_text = etSearch.getText().toString();
-                Call<TimeEntryGson> timeEntryResponse = retrofit.create(RestApi.class).getTimeEntryMsg(new TimeEntryMessage(mobile_no, search_text));
-                timeEntryResponse.enqueue(new Callback<TimeEntryGson>() {
+            public void onClick(View view) {
+                calendar=Calendar.getInstance();
+                int hour=calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(FundooHrSearchActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
-                    public void onResponse(Call<TimeEntryGson> call, Response<TimeEntryGson> response) {
-                        TimeEntryGson gsonData = response.body();
-                        Date date;
-                        date = DateFormater.getDate(gsonData.getData().getInTime());
-
-                        txtJson.setText(DateFormater.getCurrentDateString(date));
-                        txtTime.setText(DateFormater.getCurrentTimeString(date));
-                        etDate.setText(DateFormater.getCurrentDateString(date));
-                        etTime.setText(DateFormater.getCurrentTimeString(date));
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        etTime.setText( selectedHour + ":" + selectedMinute);
                     }
-
-                    @Override
-                    public void onFailure(Call<TimeEntryGson> call, Throwable t) {
-                        Toast.makeText(FundooHrSearchActivity.this, "Something happen wrong ...........", Toast.LENGTH_LONG).show();
-                    }
-                });
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
             }
         });
+        imgBtnEditDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });*/
+        imgBtnEditDate.setOnClickListener(this);
+        imgBtnEditTime.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view == imgBtnEditDate) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            etDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (view == imgBtnEditTime) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+            mSeconds = c.get(Calendar.SECOND);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            etTime.setText(hourOfDay + ":" + minute+ ":"+mSeconds);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }if (view == imgBtnSearch){
+            String mobile_no = "+919923911289"/*getIntent().getStringExtra("mobile")*/;
+            String search_text = etSearch.getText().toString();
+            Call<TimeEntryGson> timeEntryResponse = retrofit.create(RestApi.class).getTimeEntryMsg(new TimeEntryMessage(mobile_no, search_text));
+            timeEntryResponse.enqueue(new Callback<TimeEntryGson>() {
+                @Override
+                public void onResponse(Call<TimeEntryGson> call, Response<TimeEntryGson> response) {
+                    TimeEntryGson gsonData = response.body();
+                    Date date;
+                    date = DateFormater.getDate(gsonData.getData().getInTime());
+
+                    txtJson.setText(DateFormater.getCurrentDateString(date));
+                    txtTime.setText(DateFormater.getCurrentTimeString(date));
+                    etDate.setText(DateFormater.getCurrentDateString(date));
+                    etTime.setText(DateFormater.getCurrentTimeString(date));
+                }
+
+                @Override
+                public void onFailure(Call<TimeEntryGson> call, Throwable t) {
+                    Toast.makeText(FundooHrSearchActivity.this, "Something happen wrong ...........", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
 }
+
