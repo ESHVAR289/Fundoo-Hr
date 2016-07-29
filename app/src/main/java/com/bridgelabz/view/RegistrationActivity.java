dba3dcc7a76707e6cb9f8bdc7f30bf001c763726
ESHVAR MALI <eshvar89@hotmail.com>
2016-07-29 11:51:47 +0530

    Switched the new branch for REST call implementation using volley

--- app/src/main/java/com/bridgelabz/view/RegistrationActivity.java
+++ app/src/main/java/com/bridgelabz/view/RegistrationActivity.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.view;
 
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
@@ -9,7 +10,6 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,8 +18,13 @@ import com.bridgelabz.R;
 public class RegistrationActivity extends AppCompatActivity {
     private EditText etMobileNo, etEmailId, etPswd, etRePswd;
     private AppCompatButton btnSignUp;
-    private TextInputLayout inputMobileNo, inputEmailId, inputPswd, inputRePswd;
-
+    private CoordinatorLayout mCoordinatorLayout;
+    private String snackMobileNoMsg, snackEmailMsg, snackPswdMsg;
+    private StringBuilder snackBarMsg;
+    private static boolean mobileValidationFlag=false;
+    private static boolean emailValidationFlag = false;
+    private static boolean pswdValidationFlag=false;
+    private static boolean rePswdValidationFlag=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,122 +35,136 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     private void initializeTheView() {
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
         etMobileNo = (EditText) findViewById(R.id.etMobileNo);
         etEmailId = (EditText) findViewById(R.id.etEmailId);
         etPswd = (EditText) findViewById(R.id.etRegistrationPswd);
         etRePswd = (EditText) findViewById(R.id.etRegistrationRePswd);
 
-        inputMobileNo = (TextInputLayout) findViewById(R.id.inputMobileNo);
-        inputEmailId = (TextInputLayout) findViewById(R.id.inputEmailId);
-        inputPswd = (TextInputLayout) findViewById(R.id.inputPswd);
-        inputRePswd = (TextInputLayout) findViewById(R.id.inputRePswd);
+       /* etMobileNo.addTextChangedListener(new CustomTextWatcher(etMobileNo));
+        etEmailId.addTextChangedListener(new CustomTextWatcher(etEmailId));
+        etPswd.addTextChangedListener(new CustomTextWatcher(etPswd));
+        etRePswd.addTextChangedListener(new CustomTextWatcher(etRePswd));*/
+
+        Snackbar snackbar = Snackbar
+                .make(mCoordinatorLayout, "Please fill your credentials", Snackbar.LENGTH_LONG);
+        snackbar.show();
+        snackBarMsg = new StringBuilder();
 
         btnSignUp = (AppCompatButton) findViewById(R.id.btnSignUp);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                submitRegistrationDetails();
+                validateMobileNo();
+                validateEmail();
+                validatePassword();
+                if (etMobileNo.getText().toString().isEmpty() ||
+                        etEmailId.getText().toString().isEmpty() ||
+                        etPswd.getText().toString().isEmpty() ||
+                        etRePswd.getText().toString().isEmpty()) {
+                    Snackbar snackbar = Snackbar
+                            .make(mCoordinatorLayout, "Please fill the empty fields first", Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                } else {
+                    if (mobileValidationFlag || emailValidationFlag || pswdValidationFlag){
+                        Snackbar snackbar = Snackbar
+                                .make(mCoordinatorLayout, snackBarMsg.toString(), Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    }
+                    else{
+                        Snackbar snackbar = Snackbar
+                                .make(mCoordinatorLayout, "Registered successfully....!", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    }
+                }
             }
         });
     }
 
-    private void submitRegistrationDetails() {
-        if (!validateMobileNo("+91" + etMobileNo.getText().toString())) {
+    /*private boolean submitRegistrationDetails() {
+        snackBarMsg = new StringBuilder("Check your ");
+        if (!validateMobileNo(etMobileNo.getText().toString())) {
             return;
         }
-
         if (!validateEmail()) {
             return;
         }
-
-        if (!validatePassword(etPswd.getText().toString().trim(), etPswd)) {
+        if (!validatePassword(etPswd.getText().toString(), etRePswd.getText().toString())) {
             return;
         }
-        if (!validatePassword(etRePswd.getText().toString().trim(), etRePswd)) {
-            return;
-        }
-        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
-    }
-
-    private boolean validateMobileNo(String mobileNo) {
-        String regexString = "^[+][0-9]{10,13}$";
-        if (mobileNo.trim().isEmpty() && mobileNo.length() < 10 && mobileNo.length() > 13 && mobileNo.matches(regexString)) {
-            inputMobileNo.setError(getString(R.string.err_msg_mobile));
-            requestFocus(etMobileNo);
+        if (validateMobileNo(etMobileNo.getText().toString()) ||
+            validateEmail() ||
+            validatePassword(etPswd.getText().toString(), etRePswd.getText().toString())){
             return false;
-        } else {
-            inputMobileNo.setErrorEnabled(false);
         }
+
+        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
         return true;
+    }*/
+
+    private void validateMobileNo() {
+        String mobileNo =etMobileNo.getText().toString();
+        String regexString = "^[+][0-9]{10,13}$", mobileWithCountryCode = "+91" + mobileNo;
+
+        if (mobileNo.length() < 10 || mobileWithCountryCode.length() > 13 || !mobileWithCountryCode.matches(regexString)) {
+            snackBarMsg.append("Mobile No. ");
+            mobileValidationFlag = true;
+        }else
+            mobileValidationFlag = false;
     }
 
-    private boolean validateEmail() {
+    private void validateEmail() {
         String email = etEmailId.getText().toString().trim();
 
-        if (email.isEmpty() || !isValidEmail(email)) {
-            inputEmailId.setError(getString(R.string.err_msg_email));
-            requestFocus(etEmailId);
-            return false;
-        } else {
-            inputEmailId.setErrorEnabled(false);
-        }
-
-        return true;
+        if (!isValidEmail(email)) {
+            snackBarMsg.append("Email Id ");
+            emailValidationFlag = true;
+        }else
+            emailValidationFlag = false;
     }
 
-    private boolean validatePassword(String pswd, View view) {
-        if (pswd.isEmpty()) {
-            inputPswd.setError(getString(R.string.err_msg_password));
-            requestFocus(view);
-            return false;
-        } else {
-            inputPswd.setErrorEnabled(false);
-        }
-
-        return true;
+    private void validatePassword() {
+        String pswd = etPswd.getText().toString();
+        String rePswd =etRePswd.getText().toString();
+        if (!pswd.equals(rePswd)) {
+            snackBarMsg.append("Password not match ");
+            pswdValidationFlag = true;
+        }else
+            pswdValidationFlag = false;
     }
 
     private boolean isValidEmail(String email) {
         return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    private void requestFocus(View view) {
-        if (view.requestFocus()) {
-            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-        }
-    }
-
-    class CustomTextWatcher implements TextWatcher {
-
-        private View view;
-
-        private CustomTextWatcher(View view) {
-            this.view = view;
-        }
-
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        public void afterTextChanged(Editable editable) {
-            switch (view.getId()) {
-                case R.id.etMobileNo:
-                    validateMobileNo("+91" + etMobileNo.getText().toString());
-                    break;
-                case R.id.etEmailId:
-                    validateEmail();
-                    break;
-                case R.id.etRegistrationPswd:
-                    validatePassword(etPswd.getText().toString().trim(), etPswd);
-                    break;
-                case R.id.etRegistrationRePswd:
-                    validatePassword(etRePswd.getText().toString().trim(), etRePswd);
-                    break;
-            }
-        }
-
-    }
+//    private class CustomTextWatcher implements TextWatcher {
+//
+//        private View view;
+//
+//        private CustomTextWatcher(View view) {
+//            this.view = view;
+//        }
+//
+//        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//        }
+//
+//        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//        }
+//
+//        public void afterTextChanged(Editable editable) {
+//            switch (view.getId()) {
+//                case R.id.etMobileNo:
+//                    validateMobileNo("+91"+etMobileNo.getText().toString());
+//                    break;
+//                case R.id.etEmailId:
+//                    validateEmail();
+//                    break;
+//                case R.id.etRegistrationRePswd:
+//                    validatePassword(etPswd.toString(), etRePswd.toString());
+//                    break;
+//            }
+//        }
+//
+//    }
 
 }
